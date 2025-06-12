c018c2e62e8e7ebd5a87e6fc39cf153f36a3dc98
richiii <richiii@umich.edu>
2025-06-12 15:20:35 -0400

    modular sensors and files (still only 1 sensor for now)

--- main.py
+++ main.py
@@ -6,26 +6,41 @@ import time
 
 import pexpect # Module which manages subprocess I/O
 
+# Increase delay for slower computers. Eventually iterDelay is measured in minutes so it's okay
 initDelay = 10 # Initial delay after starting LLM to wait for it to be ready for input
-iterDelay = 5 # Increase delay for slower computers and/or to slow down iterations of prompting
+iterDelay = 5 # delay for each iteration of prompting
 startTime = time.time()
 
-faceTrackerFile = open('faceTracker.txt', 'r+')
-llmFile = open('llmFile.txt', 'r+')
+# The AI model itself
+llmFile = open('llmFile.txt', 'r+') # If needed, change logfile param to redirect outputs to this file instead
+llm = pexpect.spawn('ollama run llama3.2', encoding='utf-8', logfile=sys.stdout)
 
-faceTracker = pexpect.spawn('python -u ./Sensors/PythonFaceTracker/main.py', encoding='utf-8', logfile=faceTrackerFile)
-llm = pexpect.spawn('ollama run llama3.2:1b', encoding='utf-8', logfile=sys.stdout) # Change logfile param to redirect outputs (e.g. to a file instead)
+logFiles = [ # Log files, sensor output is periodically read from here and given to the AI
+    open('faceTracker.txt', 'r+')
+    # Sensor 2 file ...
+    # Sensor 3 file ...
+]
 
-time.sleep(initDelay)
-while faceTracker.status:
-    # Get most recent output (TODO eventually change to seeking from end)
-    faceTrackerFile.seek(0)
-    out = faceTrackerFile.readlines()[-1]
+sensors = [ # Sensors which record data to be passed to the AI
+    pexpect.spawn('python -u ./Sensors/PythonFaceTracker/main.py', encoding='utf-8', logfile=logFiles[0])
+    # Sensor 2 process ...
+    # Sensor 3 process ...
+]
 
-    llm.sendline(out) # Write the facetracker output to the LLM's input
+time.sleep(initDelay) # Main loop
+while sensors[0].status: # TODO how to close? For now just 'q' on FaceTracker to close everything
+    sensorData = "Aggregated Sensor data (Each sensor on newline):"
+    for f in logFiles: # Get most recent output per sensor 
+        f.seek(0) # TODO eventually change to seeking from end of file instead of start
+        sensorData += f.readlines()[-1]
+
+    # TODO Multiline prompt has to start & end with triple quotes (""") I believe
+    llm.sendline(sensorData) # Write the facetracker output to the LLM's input
     llm.expect('>>>') # Stream the AI's output while awaiting the end of the response
 
-faceTrackerFile.close() # Close files and terminate
-llmFile.close()
-faceTracker.terminate()
+    time.sleep(iterDelay) # Delay AFTER response (So you can actually read it)
+
+llmFile.close() # Close files and terminate
 llm.terminate()
+for f in logFiles: f.close() 
+for s in sensors: s.terminate()
