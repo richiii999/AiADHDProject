814c1a1b020cd59269eb3c51a4709ed4e38da628
richiii <richiii@umich.edu>
2025-08-21 15:15:55 -0400

    Update API.py anthropic instead of the guys name

--- API.py
+++ API.py
@@ -17,7 +17,7 @@ with open('.webui_admin_key', 'r+') as f: # Re-open in read/write mode
 
 localHostPort = "8080"
 defaultHeader = {'Authorization':f'Bearer {adminToken}','Content-Type':'application/json'}
-Models = ["justinrahb.claude-3-7-sonnet-20250219"] # default Cloud-based model
+Models = ["anthropic.claude-3-7-sonnet-20250219"] # default Cloud-based model
 
 
 # Thanks to https://github.com/open-webui/open-webui/discussions/11761
